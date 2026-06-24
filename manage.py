46564d829da8881467d9a245195fdc017b306805
Prudhvinadh03 <prudhvinadh@flyingduck.io>
2026-06-24 18:58:52 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 import os
 import django
-# from django import forms
+from django import forms
 from django.utils.encoding import uri_to_iri
-# from django.utils.text import Truncator
+from django.utils.text import Truncator
 
 # Set up Django environment
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')  # Replace 'your_project.settings' with your settings module
@@ -15,22 +15,22 @@ def main():
     print("Converted IRI:", iri)  # Expected output: https://example.com/path/école
 
     # Part 2: Django form with multiple file uploads
-    # class UploadMultipleFilesForm(forms.Form):
-    #     files = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    class UploadMultipleFilesForm(forms.Form):
+        files = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
 
-    # form = UploadMultipleFilesForm()
-    # print("Form HTML:")
-    # print(form.as_p())
+    form = UploadMultipleFilesForm()
+    print("Form HTML:")
+    print(form.as_p())
     
-    # long_text = "This is an example of a long text that will be truncated to a specific number of characters."
+    long_text = "This is an example of a long text that will be truncated to a specific number of characters."
 
-    # # Create a Truncator instance
-    # t = Truncator(long_text)
+    # Create a Truncator instance
+    t = Truncator(long_text)
 
-    # # Correctly call chars() with the desired character limit
-    # truncated_html = t.chars(30, html=True)
+    # Correctly call chars() with the desired character limit
+    truncated_html = t.chars(30, html=True)
 
-    # print("Truncated text:", truncated_html)
+    print("Truncated text:", truncated_html)
 
 if __name__ == "__main__":
     main()
