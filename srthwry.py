72145746de8b720068e9118b46af163c57e2849b
Prudhvinadh <prudhvinadh@flyingduck.io>
2026-07-09 15:34:23 +0530

    added project

---
+++ srthwry.py
@@ -0,0 +1,225 @@
+g=[
+        {
+            "SPDXID": "SPDXRef-SQLPARSE-0.5.5",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "sqlparse",
+            "supplier": "NOASSERTION",
+            "versionInfo": "0.5.5"
+        },
+        {
+            "SPDXID": "SPDXRef-ASGIREF-3.11.1",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "asgiref",
+            "supplier": "Person: Django Software Foundation (foundation@djangoproject.com)",
+            "versionInfo": "3.11.1"
+        },
+        {
+            "SPDXID": "SPDXRef-DJANGO-4.1.3",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "Django",
+            "supplier": "Person: Django Software Foundation (foundation@djangoproject.com)",
+            "versionInfo": "4.1.3"
+        },
+        {
+            "SPDXID": "SPDXRef-ASN1CRYPTO-1.5.1",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "asn1crypto",
+            "supplier": "Person: wbond (will@wbond.net)",
+            "versionInfo": "1.5.1"
+        },
+        {
+            "SPDXID": "SPDXRef-CERTIFI-2026.6.17",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "certifi",
+            "supplier": "Person: Kenneth Reitz (me@kennethreitz.com)",
+            "versionInfo": "2026.6.17"
+        },
+        {
+            "SPDXID": "SPDXRef-CFFI-2.1.0",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "cffi",
+            "supplier": "NOASSERTION",
+            "versionInfo": "2.1.0"
+        },
+        {
+            "SPDXID": "SPDXRef-CHARDET-3.0.4",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "chardet",
+            "supplier": "Person: Daniel Blanchard (dan.blanchard@gmail.com)",
+            "versionInfo": "3.0.4"
+        },
+        {
+            "SPDXID": "SPDXRef-CLICK-8.4.2",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "click",
+            "supplier": "NOASSERTION",
+            "versionInfo": "8.4.2"
+        },
+        {
+            "SPDXID": "SPDXRef-CRYPTOGRAPHY-2.3",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "cryptography",
+            "supplier": "Person: The cryptography developers (cryptography-dev@python.org)",
+            "versionInfo": "2.3"
+        },
+        {
+            "SPDXID": "SPDXRef-FLASK-1.0",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "Flask",
+            "supplier": "Person: Armin Ronacher (armin.ronacher@active-4.com)",
+            "versionInfo": "1.0"
+        },
+        {
+            "SPDXID": "SPDXRef-IDNA-2.7",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "idna",
+            "supplier": "Person: Kim Davies (kim@cynosure.com.au)",
+            "versionInfo": "2.7"
+        },
+        {
+            "SPDXID": "SPDXRef-ITSDANGEROUS-1.1.0",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "itsdangerous",
+            "supplier": "Person: Armin Ronacher (armin.ronacher@active-4.com)",
+            "versionInfo": "1.1.0"
+        },
+        {
+            "SPDXID": "SPDXRef-JINJA2-2.10.1",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "Jinja2",
+            "supplier": "Person: Armin Ronacher (armin.ronacher@active-4.com)",
+            "versionInfo": "2.10.1"
+        },
+        {
+            "SPDXID": "SPDXRef-LXML-4.6.2",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "lxml",
+            "supplier": "Person: lxml dev team (lxml-dev@lxml.de)",
+            "versionInfo": "4.6.2"
+        },
+        {
+            "SPDXID": "SPDXRef-MARKUPSAFE-3.0.3",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "MarkupSafe",
+            "supplier": "NOASSERTION",
+            "versionInfo": "3.0.3"
+        },
+        {
+            "SPDXID": "SPDXRef-PILLOW-8.1.0",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "Pillow",
+            "supplier": "Person: Alex Clark (PIL Fork Author) (aclark@python-pillow.org)",
+            "versionInfo": "8.1.0"
+        },
+        {
+            "SPDXID": "SPDXRef-PYCPARSER-3.0",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "pycparser",
+            "supplier": "NOASSERTION",
+            "versionInfo": "3.0"
+        },
+        {
+            "SPDXID": "SPDXRef-PYYAML-5.3.1",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "PyYAML",
+            "supplier": "Person: Kirill Simonov (xi@resolvent.net)",
+            "versionInfo": "5.3.1"
+        },
+        {
+            "SPDXID": "SPDXRef-REQUESTS-2.19.1",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "requests",
+            "supplier": "Person: Kenneth Reitz (me@kennethreitz.org)",
+            "versionInfo": "2.19.1"
+        },
+        {
+            "SPDXID": "SPDXRef-SIX-1.17.0",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "six",
+            "supplier": "Person: Benjamin Peterson (benjamin@python.org)",
+            "versionInfo": "1.17.0"
+        },
+        {
+            "SPDXID": "SPDXRef-URLLIB3-1.23",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "urllib3",
+            "supplier": "Person: Andrey Petrov (andrey.petrov@shazow.net)",
+            "versionInfo": "1.23"
+        },
+        {
+            "SPDXID": "SPDXRef-WERKZEUG-0.15.4",
+            "downloadLocation": "NOASSERTION",
+            "filesAnalyzed": False,
+            "homepage": "NOASSERTION",
+            "licenseDeclared": "NOASSERTION",
+            "name": "Werkzeug",
+            "supplier": "Person: Armin Ronacher (armin.ronacher@active-4.com)",
+            "versionInfo": "0.15.4"
+        }
+    ]
+
+
+print(len(g))
