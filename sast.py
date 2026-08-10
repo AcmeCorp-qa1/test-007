c5156acfb159dc2dfea2459307edc0b8743e82bb
Prudhvinadh03 <prudhvinadh@flyingduck.io>
2026-08-10 12:02:53 +0530

    Update sast.py

--- sast.py
+++ sast.py
@@ -14,7 +14,8 @@ def init_db():
     conn.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)''')
     conn.execute('''CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY, user_id INTEGER, title TEXT, content TEXT)''')
     conn.close()
- 
+
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
